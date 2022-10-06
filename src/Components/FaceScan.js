95a66e90e72ce0f09d3811389addd48ada63ea68
Md. Nazim Hasan <19-40118-1@student.aiub.edu>
2022-10-06 15:53:11 +0600

    NID Details, Capture camera added

--- src/Components/FaceScan.js
+++ src/Components/FaceScan.js
@@ -1,91 +1,93 @@
-import * as React from "react";
+import * as React from 'react';
 
-import { TouchableOpacity, StyleSheet, View, Text,Button } from "react-native";
+import {TouchableOpacity, StyleSheet, View, Text, Button} from 'react-native';
+import {Card} from 'react-native-paper';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import NID from "../../assets/images/face.png";
+import NID from '../../assets/images/face.png';
 
-const FaceScan = (props) => {
-  return(
+const FaceScan = props => {
+  return (
     <View style={styles.container}>
-<View>
-  <Text>NID CRAD Img Here </Text>
-  <Text>NID CRAD Information Here </Text>
-</View>
+      {/* <View>
+        <Text>face id scan Contents</Text>
+        <Button
+          title="Registration"
+          onPress={() => props.navigation.navigate('RegistrationForm')}
+        />
+      </View> */}
 
-
-<View>
-  <Text>face id scan Contents</Text>
-  <Button title='Registration' onPress={()=>props.navigation.navigate('RegistrationForm')} />
-  
-</View>
-
-
-
-
-
-
-
-
-
-      {/* <Text style={styles.nidComment}>Place your face to take a clear photo</Text>
-      <Card style={{marginTop: 30, marginBottom: 365, width: "90%"}}>
+      <Text style={styles.nidComment}>
+        Place your face to take a clear photo
+      </Text>
+      <Card style={{marginTop: 30, marginBottom: 365, width: '90%'}}>
         <Card.Cover style={styles.personNID} source={NID} />
         <Card.Actions style={styles.nidActions}>
           <TouchableOpacity>
-            <Button labelStyle={{ fontSize: 35 }} icon="swap-horizontal"/>
-       
+            <Button labelStyle={{fontSize: 35}} icon="camera" title="camera" />
           </TouchableOpacity>
           <TouchableOpacity>
-            <Button labelStyle={{ fontSize: 35 }} icon="camera"/>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Button labelStyle={{ fontSize: 35 }} icon="camera-retake-outline"/>
+            <Button
+              labelStyle={{fontSize: 35}}
+              icon="camera-retake-outline"
+              title="retake"
+            />
           </TouchableOpacity>
         </Card.Actions>
-        <View style={{ justifyContent: 'center', alignItems: 'center', width:"100%"}}>
-              <TouchableOpacity style={styles.button} onPress={() => {
-                  props.navigation.navigate("Home");
-              }}>
-                  <Text style={{textAlign:"center", color: "white", fontSize: 20}}>Next</Text>
-              </TouchableOpacity>
-          </View>
-      </Card> */}
+        <View
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100%',
+          }}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              props.navigation.navigate('RegistrationForm');
+            }}>
+            <Text style={{textAlign: 'center', color: 'white', fontSize: 20}}>
+              Next
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </Card>
     </View>
-  )
-}
+  );
+};
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    width: "100%"
-    },
-    nidComment:{
-        color: 'gray',
-        fontSize: 20,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 70
-    },
-    personNID:{
-    height:  210,
-    width: 320,
-
-    },
-    nidActions:{
-    flexDirection:'row',
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#F5FCFF',
+  },
+  nidComment: {
+    color: 'gray',
+    fontSize: 20,
     justifyContent: 'center',
-    alignItems: 'center'
-},
-button: {
-    backgroundColor: "#2a24c9",
-    justifyContent: "center",
-    alignItems: "center",
+    alignItems: 'center',
+    marginTop: 70,
+  },
+  personNID: {
+    height: 210,
+    width: 320,
+  },
+  nidActions: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  button: {
+    backgroundColor: '#2a24c9',
+    justifyContent: 'center',
+    alignItems: 'center',
     height: 40,
-    width: "70%",
+    width: '70%',
     borderRadius: 10,
-    marginBottom: 15
-},
+    marginBottom: 15,
+  },
 });
 
 export default FaceScan;
